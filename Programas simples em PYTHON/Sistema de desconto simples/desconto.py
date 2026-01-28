608301e3d8047dc3e53632da6738bf23fc5d1b4b
ritaalice2025sp-stack <ritaalice2025.sp@gmail.com>
2026-01-28 16:07:38 -0300

    Add discount calculation and output formatting

--- Programas simples em PYTHON/Sistema de desconto simples/desconto.py
+++ Programas simples em PYTHON/Sistema de desconto simples/desconto.py
@@ -1,6 +1,10 @@
-# Programa que pergunta o valor inicial de um produto, para ser dado o desconto de 5%.
+# Entrada do valor
 valor_inicial = float(input('Qual o valor do produto? '))
+
+# Lógica do desconto
 desconto = valor_inicial * 0.05
 valor_com_desconto = valor_inicial - desconto
+
+# Resultado
 print('O valor incial do produto era de {:.2f} e após recerber 5% de desconto ficou {:.2f}.'.format(valor_inicial, valor_com_desconto))
 # Sempre coloco um limite de casas depos da vírgula nos valores que vão aparecer afim de ficar mais limpo.
